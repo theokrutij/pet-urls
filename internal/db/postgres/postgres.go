e0c89b8e3d2c88cc63dd844bb3873746971df67f
Fedor Krutiy <theokrutij@yandex.ru>
2025-06-26 14:32:03 +0300

    add shortener repository implementation

---
+++ internal/db/postgres/postgres.go
@@ -0,0 +1,93 @@
+package postgres
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	maxAttempts = 3
+	delay       = 100 * time.Millisecond
+)
+
+type postgresConnectionPool struct {
+	*pgxpool.Pool
+}
+
+type Config struct {
+	DSN string
+}
+
+func New(ctx context.Context, config *Config) (*postgresConnectionPool, error) {
+	pgxConfig, err := pgxpool.ParseConfig(config.DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, pgxConfig)
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		<-ctx.Done()
+		pool.Close()
+	}()
+
+	return &postgresConnectionPool{pool}, nil
+}
+
+func withRetry(ctx context.Context, fn func(context.Context) error) error { // TODO: review and refactor, if needed
+	var lastErr error
+	for attempt := 1; attempt <= maxAttempts; attempt++ {
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+
+		err := fn(ctx)
+		if err == nil {
+			return nil
+		}
+
+		lastErr = err
+
+		if !isRetryableError(err) {
+			return err // fatal error
+		}
+
+		// exponential backoff (optional)
+		sleep := time.Duration(attempt) * delay
+		select {
+		case <-time.After(sleep):
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+	return fmt.Errorf("operation failed after %d attempts: %w", maxAttempts, lastErr)
+}
+
+func isRetryableError(err error) bool {
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		switch pgErr.Code {
+		case pgerrcode.SerializationFailure, pgerrcode.DeadlockDetected:
+			return true
+		}
+	}
+	return false
+}
+
+func isNotFoundError(err error) bool {
+	return errors.Is(err, pgx.ErrNoRows)
+}
+
+func isNotUniqueError(err error) bool {
+	var pgErr *pgconn.PgError
+	return errors.As(err, &pgErr) && pgErr.Code == pgerrcode.UniqueViolation
+}
---
+++ internal/db/postgres/shortenerRepo.go
@@ -0,0 +1,83 @@
+package postgres
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/theokrutij/pet-urls/internal/services/shortener"
+)
+
+type shortenerRepository struct {
+	p *postgresConnectionPool
+}
+
+func NewShortenerRepository(p *postgresConnectionPool) *shortenerRepository {
+	return &shortenerRepository{p}
+}
+
+func (s *shortenerRepository) HealthCheck(ctx context.Context) error {
+	return s.p.Ping(ctx)
+}
+
+func (s *shortenerRepository) SaveToken(ctx context.Context, token shortener.URLToken) error {
+	const query = `
+		INSERT INTO url_tokens (token, url, valid_until)
+		VALUES ($1, $2, $3)
+	`
+	err := withRetry(ctx, func(ctx context.Context) error {
+		_, err := s.p.Exec(ctx, query, token.Token, token.URL, token.ExpiresAt)
+		return err
+	})
+
+	if err != nil {
+		return fmt.Errorf("postgres, saving token: %w", err)
+	}
+
+	return nil
+}
+
+type notFoundError struct {
+	inner error
+}
+
+func (n *notFoundError) Error() string {
+	return "not found"
+}
+
+func (n *notFoundError) Unwrap() error {
+	return n.inner
+}
+
+func (n *notFoundError) NotFound() bool {
+	return true
+}
+
+func (s *shortenerRepository) GetToken(ctx context.Context, tokenStr string) (shortener.URLToken, error) {
+	const query = `
+		SELECT url, valid_until FROM url_tokens
+		WHERE token = $1
+	`
+	token := shortener.URLToken{Token: shortener.Token(tokenStr)}
+	err := withRetry(ctx, func(ctx context.Context) error {
+		return s.p.QueryRow(ctx, query, tokenStr).Scan(&token.URL, &token.ExpiresAt)
+	})
+
+	if isNotFoundError(err) {
+		return token, fmt.Errorf("postgres, fetching token: %w", &notFoundError{err})
+	} else if err != nil {
+		return token, fmt.Errorf("postgres, fetching token: %w", err)
+	}
+
+	return token, nil
+}
+
+func (s *shortenerRepository) DeleteToken(ctx context.Context, token string) error {
+	const query = `
+		DELETE FROM url_tokens
+		WHERE token = $1
+	`
+	return withRetry(ctx, func(ctx context.Context) error {
+		_, err := s.p.Exec(ctx, query, token)
+		return err
+	})
+}
