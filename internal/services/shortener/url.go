e0f9ecfbce5170ec71df05b4bd8612b425bfd7ed
Fedor Krutiy <theokrutij@yandex.ru>
2025-06-29 10:45:28 +0300

    update normalizeHTTP, validate domain name as ip / FQDN

--- internal/services/shortener/url.go
+++ internal/services/shortener/url.go
@@ -1,36 +1,38 @@
 package shortener
 
 import (
-	"fmt"
+	"errors"
+	"net"
 	"net/url"
 	"strings"
+	"unicode/utf8"
 )
 
+// TODO:
+//   - escape all NON-ASCII
+//   - add HTTP schema
+//   - remove fragment
+//   - toLower
+//   - cover with tests
 func normalizeHTTP(raw string) (URL, error) {
+	if utf8.RuneCountInString(raw) > 2000 {
+		return "", errors.New("too long url")
+	}
 	if !strings.Contains(raw, "://") {
 		raw = "http://" + raw
 	}
 	u, err := url.Parse(raw)
 	if err != nil {
-		return "", fmt.Errorf("invalid url")
-	}
-	if u.Scheme == "" {
-		raw = "http://" + raw
-		u, err = url.Parse(raw)
-		if err != nil {
-			return "", fmt.Errorf("invalid url")
-		}
+		return "", errors.New("invalid url")
 	}
 	if !urlIsValid(u) {
-		return "", fmt.Errorf("invalid url")
+		return "", errors.New("invalid url")
 	}
 	u.Scheme = strings.ToLower(u.Scheme)
 
 	return URL(u.String()), nil
 }
 
-// check that Host is either ipv4 or ipv6 or FQDN
-// limit length?
 func urlIsValid(u *url.URL) bool {
 	if u.Scheme != "http" && u.Scheme != "https" {
 		return false
@@ -41,8 +43,32 @@ func urlIsValid(u *url.URL) bool {
 	if u.User != nil {
 		return false
 	}
-	if u.Host == "" {
+	if !isFQDN(u.Host) && !isIP(u.Host) {
 		return false
 	}
 	return true
+}
+
+// naive: only allow ascii letters in labels
+func isFQDN(s string) bool {
+	s, _ = strings.CutSuffix(s, ".")
+	labels := strings.Split(s, ".")
+	for _, label := range labels {
+		if len(label) < 1 || len(label) > 63 {
+			return false
+		}
+		for _, r := range label {
+			if r < 'A' || (r > 'Z' && r < 'a') || r > 'z' {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func isIP(s string) bool {
+	s = strings.Trim(s, "[]")
+	ip := net.ParseIP(s)
+	return ip != nil && ip.To16() != nil
 }
