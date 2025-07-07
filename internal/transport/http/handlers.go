1209e975251b536d69d95af7c039620dc8dc7a49
Fedor Krutiy <theokrutij@yandex.ru>
2025-07-07 08:20:10 +0300

    handler.go readability patch

--- internal/transport/http/handlers.go
+++ internal/transport/http/handlers.go
@@ -1,7 +1,6 @@
 package http
 
 import (
-	"encoding/base64"
 	"errors"
 	"net/http"
 	"time"
@@ -79,160 +78,6 @@ func (s *server) handleResolveToken() http.HandlerFunc {
 	}
 }
 
-func tokenFromPath(r *http.Request) string {
-	return r.PathValue("token")
-}
-
-func (s *server) handleLogin(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("Content-Type") != "application/x-www-form-urlencoded" {
-		writeError(w, codeInvalidRequest, "expected application/x-www-form-urlencoded content-type")
-		return
-	}
-	if err := r.ParseForm(); err != nil {
-		writeError(w, codeInvalidRequest, "unparsable form")
-		return
-	}
-
-	login := r.PostForm.Get("login")
-	if login == "" {
-		writeError(w, codeInvalidParameter, "login empty or missing")
-		return
-	}
-	password := r.PostForm.Get("password")
-	if password == "" {
-		writeError(w, codeInvalidParameter, "password empty or missing")
-		return
-	}
-
-	refreshToken, accessToken, err := s.auth.Login(r.Context(), login, password)
-	if errors.Is(err, auth.ErrInvalidCredentials) {
-		writeError(w, codeUnauthenticated, "invalid credentials")
-		return
-	} else if err != nil {
-		writeError(w, codeInternalError, "internal")
-		return
-	}
-
-	s.setRefreshTokenCookie(w, refreshToken)
-	writeAccessTokenAsJSON(w, accessToken)
-}
-
-func writeAccessTokenAsJSON(w http.ResponseWriter, token auth.AccessToken) {
-	var response = struct {
-		AccessToken string `json:"access_token"`
-	}{
-		AccessToken: string(token),
-	}
-	writeResponseAsJSON(w, response, 200)
-}
-
-const (
-	refreshTokenCookieName = "refreshToken"
-	refreshTokenCookiePath = "/api/v1/auth"
-)
-
-func (s *server) setRefreshTokenCookie(w http.ResponseWriter, token []byte) {
-	cookie := http.Cookie{
-		Name:     refreshTokenCookieName,
-		Value:    base64.URLEncoding.EncodeToString(token),
-		Path:     refreshTokenCookiePath,
-		MaxAge:   int(s.auth.RefreshTokenTTL().Seconds()),
-		Secure:   !s.debug, // allow cookies over http for debugging
-		HttpOnly: true,
-		SameSite: http.SameSiteStrictMode,
-	}
-	http.SetCookie(w, &cookie)
-}
-
-func (s *server) expireRefreshTokenCookie(w http.ResponseWriter) {
-	cookie := http.Cookie{
-		Name:   refreshTokenCookieName,
-		Value:  "",
-		Path:   refreshTokenCookiePath,
-		MaxAge: -1,
-	}
-	http.SetCookie(w, &cookie)
-}
-
-func (s *server) handleRegistration(w http.ResponseWriter, r *http.Request) {
-	type requestSchema struct {
-		Login    string `json:"login"`
-		Password string `json:"password"`
-	}
-
-	request, err := readBodyAsJSON[requestSchema](r)
-	if err != nil {
-		writeError(w, codeInvalidRequest, "not parsable")
-		return
-	}
-	if request.Login == "" {
-		writeError(w, codeInvalidParameter, "login empty or missing")
-		return
-	}
-	if request.Password == "" {
-		writeError(w, codeInvalidParameter, "password empty or missing")
-		return
-	}
-
-	err = s.auth.Register(r.Context(), request.Login, request.Password)
-	if errors.Is(err, auth.ErrInvalidLogin) {
-		writeError(w, codeInvalidParameter, "invalid login")
-		return
-	} else if errors.Is(err, auth.ErrInvalidPassword) {
-		writeError(w, codeInvalidParameter, "invalid password")
-		return
-	} else if errors.Is(err, auth.ErrLoginNotUnique) {
-		writeError(w, codeInvalidParameter, "login already exists")
-		return
-	} else if err != nil {
-		writeError(w, codeInternalError, "")
-		return
-	}
-
-	w.WriteHeader(201)
-}
-
-func (s *server) handleTokenRefresh(w http.ResponseWriter, r *http.Request) {
-	refreshToken, err := getRefreshTokenFromCookie(r)
-	if err != nil {
-		writeError(w, codeUnauthenticated, "unauthenticated")
-		return
-	}
-
-	accessToken, err := s.auth.Refresh(r.Context(), refreshToken)
-	if err != nil {
-		writeError(w, codeUnauthenticated, "unauthenticated")
-		return
-	}
-
-	writeAccessTokenAsJSON(w, accessToken)
-}
-
-func getRefreshTokenFromCookie(r *http.Request) ([]byte, error) {
-	cookie, err := r.Cookie(refreshTokenCookieName)
-	if err != nil {
-		return nil, err
-	}
-
-	return base64.URLEncoding.DecodeString(cookie.Value)
-}
-
-func (s *server) handleLogout(w http.ResponseWriter, r *http.Request) {
-	refreshToken, err := getRefreshTokenFromCookie(r)
-	if err != nil {
-		writeError(w, codeUnauthenticated, "unauthenticated")
-		return
-	}
-
-	err = s.auth.Logout(r.Context(), refreshToken)
-	if err != nil {
-		writeError(w, codeInternalError, "internal")
-		return
-	}
-
-	s.expireRefreshTokenCookie(w)
-}
-
 func (s *server) handleCreateTokenWithOwner(w http.ResponseWriter, r *http.Request) {
 	type requestSchema struct {
 		Token string `json:"token"`
@@ -304,4 +149,121 @@ func (s *server) handleDeleteToken(w http.ResponseWriter, r *http.Request) {
 	} else if err != nil {
 		writeError(w, codeInternalError, "internal")
 	}
+}
+
+func tokenFromPath(r *http.Request) string {
+	return r.PathValue("token")
+}
+
+func (s *server) handleRegistration(w http.ResponseWriter, r *http.Request) {
+	type requestSchema struct {
+		Login    string `json:"login"`
+		Password string `json:"password"`
+	}
+
+	request, err := readBodyAsJSON[requestSchema](r)
+	if err != nil {
+		writeError(w, codeInvalidRequest, "not parsable")
+		return
+	}
+	if request.Login == "" {
+		writeError(w, codeInvalidParameter, "login empty or missing")
+		return
+	}
+	if request.Password == "" {
+		writeError(w, codeInvalidParameter, "password empty or missing")
+		return
+	}
+
+	err = s.auth.Register(r.Context(), request.Login, request.Password)
+	if errors.Is(err, auth.ErrInvalidLogin) {
+		writeError(w, codeInvalidParameter, "invalid login")
+		return
+	} else if errors.Is(err, auth.ErrInvalidPassword) {
+		writeError(w, codeInvalidParameter, "invalid password")
+		return
+	} else if errors.Is(err, auth.ErrLoginNotUnique) {
+		writeError(w, codeInvalidParameter, "login already exists")
+		return
+	} else if err != nil {
+		writeError(w, codeInternalError, "")
+		return
+	}
+
+	w.WriteHeader(201)
+}
+
+func (s *server) handleLogin(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Content-Type") != "application/x-www-form-urlencoded" {
+		writeError(w, codeInvalidRequest, "expected application/x-www-form-urlencoded content-type")
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		writeError(w, codeInvalidRequest, "unparsable form")
+		return
+	}
+
+	login := r.PostForm.Get("login")
+	if login == "" {
+		writeError(w, codeInvalidParameter, "login empty or missing")
+		return
+	}
+	password := r.PostForm.Get("password")
+	if password == "" {
+		writeError(w, codeInvalidParameter, "password empty or missing")
+		return
+	}
+
+	refreshToken, accessToken, err := s.auth.Login(r.Context(), login, password)
+	if errors.Is(err, auth.ErrInvalidCredentials) {
+		writeError(w, codeUnauthenticated, "invalid credentials")
+		return
+	} else if err != nil {
+		writeError(w, codeInternalError, "internal")
+		return
+	}
+
+	s.setRefreshTokenCookie(w, refreshToken)
+	writeAccessTokenAsJSON(w, accessToken)
+}
+
+func (s *server) handleLogout(w http.ResponseWriter, r *http.Request) {
+	refreshToken, err := getRefreshTokenFromCookie(r)
+	if err != nil {
+		writeError(w, codeUnauthenticated, "unauthenticated")
+		return
+	}
+
+	err = s.auth.Logout(r.Context(), refreshToken)
+	if err != nil {
+		writeError(w, codeInternalError, "internal")
+		return
+	}
+
+	s.expireRefreshTokenCookie(w)
+}
+
+func (s *server) handleTokenRefresh(w http.ResponseWriter, r *http.Request) {
+	refreshToken, err := getRefreshTokenFromCookie(r)
+	if err != nil {
+		writeError(w, codeUnauthenticated, "unauthenticated")
+		return
+	}
+
+	accessToken, err := s.auth.Refresh(r.Context(), refreshToken)
+	if err != nil {
+		writeError(w, codeUnauthenticated, "unauthenticated")
+		return
+	}
+
+	writeAccessTokenAsJSON(w, accessToken)
+}
+
+func writeAccessTokenAsJSON(w http.ResponseWriter, token auth.AccessToken) {
+	var response = struct {
+		AccessToken string `json:"access_token"`
+	}{
+		AccessToken: string(token),
+	}
+	writeResponseAsJSON(w, response, 200)
 }
