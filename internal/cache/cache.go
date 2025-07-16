c8aaf37400c6401ebd68545e8c2a49e9697cbea3
Fedor Krutiy <theokrutij@yandex.ru>
2025-07-16 09:57:00 +0300

    cache: export Cache type for testing

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -10,7 +10,7 @@ import (
 	"github.com/theokrutij/pet-urls/internal/services/shortener"
 )
 
-type cache struct {
+type Cache struct {
 	rdb *redis.Client
 }
 
@@ -18,7 +18,7 @@ type Config struct {
 	Addr string
 }
 
-func New(ctx context.Context, config *Config) *cache {
+func New(ctx context.Context, config *Config) *Cache {
 	rdb := redis.NewClient(&redis.Options{Addr: config.Addr})
 
 	go func() {
@@ -26,12 +26,12 @@ func New(ctx context.Context, config *Config) *cache {
 		rdb.Close()
 	}()
 
-	return &cache{
+	return &Cache{
 		rdb: rdb,
 	}
 }
 
-func (c *cache) HealthCheck(ctx context.Context) error {
+func (c *Cache) HealthCheck(ctx context.Context) error {
 	pong, err := c.rdb.Ping(ctx).Result()
 	if err != nil {
 		return err
@@ -43,11 +43,11 @@ func (c *cache) HealthCheck(ctx context.Context) error {
 	return nil
 }
 
-func (c *cache) SaveToken(ctx context.Context, token shortener.Token, url shortener.URL, ttl time.Duration) error {
+func (c *Cache) SaveToken(ctx context.Context, token shortener.Token, url shortener.URL, ttl time.Duration) error {
 	return c.set(ctx, string(token), string(url), ttl)
 }
 
-func (c *cache) GetToken(ctx context.Context, tokenStr string) (shortener.URL, bool, error) {
+func (c *Cache) GetToken(ctx context.Context, tokenStr string) (shortener.URL, bool, error) {
 	v, err := c.get(ctx, tokenStr)
 	url := shortener.URL(v)
 	if errors.Is(err, redis.Nil) {
@@ -60,15 +60,15 @@ func (c *cache) GetToken(ctx context.Context, tokenStr string) (shortener.URL, b
 	return url, true, nil
 }
 
-func (c *cache) DeleteToken(ctx context.Context, tokenStr string) error {
+func (c *Cache) DeleteToken(ctx context.Context, tokenStr string) error {
 	return c.delete(ctx, tokenStr)
 }
 
-func (c *cache) set(ctx context.Context, key, value string, ttl time.Duration) error {
+func (c *Cache) set(ctx context.Context, key, value string, ttl time.Duration) error {
 	return c.rdb.Set(ctx, key, value, ttl).Err()
 }
 
-func (c *cache) get(ctx context.Context, key string) (string, error) {
+func (c *Cache) get(ctx context.Context, key string) (string, error) {
 	val, err := c.rdb.Get(ctx, key).Result()
 	if err == redis.Nil {
 		return "", fmt.Errorf("get from cache, key: %s, err: %w", key, err)
@@ -79,6 +79,6 @@ func (c *cache) get(ctx context.Context, key string) (string, error) {
 	return val, nil
 }
 
-func (c *cache) delete(ctx context.Context, key string) error {
+func (c *Cache) delete(ctx context.Context, key string) error {
 	return c.rdb.Del(ctx, key).Err()
 }
